af51e19e85d3a66057872b1ca8748485c4657e42
edmondpau <edmondpau@g.ucla.edu>
2021-09-01 13:13:21 -0700

    ECommerce project

---
+++ src/components/Cart.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Cart extends Component {
+    render() {
+        return (
+            <div>
+                <h3>CARTTTT</h3>
+            </div>
+        )
+    }
+}
